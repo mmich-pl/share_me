a74b64becd3f41284e6b8648ee2bb09fb3158a35
mmich-pl <m.michal.parzych@gmail.com>
2022-11-05 16:34:47 +0100

    create Post and Comment model

---
+++ backend/core/videos/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
+from datetime import datetime
+
+# Create your models here.
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=255)
+    postedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    postedAt = models.DateField(default=datetime.today)
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Comment_detail", kwargs={"pk": self.pk})
+
+
+class Post(models.Model):
+    captions = models.CharField(max_length=255)
+    video = models.FileField(upload_to="video/%y")
+    likes = models.ManyToManyField(User, related_name='video_like')
+    coments = models.ManyToManyField(Comment, related_name='video_comment')
+    postedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Post_detail", kwargs={"pk": self.pk})
